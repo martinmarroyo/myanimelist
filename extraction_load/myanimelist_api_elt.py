7cc4bccd5aa4331a320e710c1100c16bbade5d9f
Martin Arroyo <marroyo7775@gmail.com>
2022-05-11 23:06:32 -0400

    Reformatted and linted code

--- extraction_load/myanimelist_api_elt.py
+++ extraction_load/myanimelist_api_elt.py
@@ -1,75 +1,87 @@
-import requests
-import json
+""""
+A script for extracting data from the Jikkan anime
+API and loading it to Postgres
+"""
 import time
-import timeit
-import psycopg2
 import logging
-import myanimelist_api_functions
-import pandas as pd
-from itertools import count
-from datetime import datetime
+import requests
+import psycopg2
 from dotenv import dotenv_values
-from psycopg2 import pool
+from myanimelist_api_functions import (
+    get_page_count,
+    generate_anime_list,
+    add_anime,
+    upload_anime_stats,
+    get_anime_ids,
+    insert_anime_scores_and_stats,
+    refresh_views,
+    clear_staging
+)
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
 
-    conf = dotenv_values('.env')
-    logging.basicConfig(level=logging.INFO,
-                        filename="animelistapi.log",
-                        encoding='utf-8',
-                        format='%(asctime)s:%(levelname)s:%(message)s')
-
+    conf = dotenv_values(".env")
+    logging.basicConfig(
+        level=logging.INFO,
+        filename="animelistapi.log",
+        encoding="utf-8",
+        format="%(asctime)s:%(levelname)s:%(message)s"
+    )
 
     logging.info("Starting...")
     start = time.time()
 
     # Set up connections to database and API
-    connection_pool = psycopg2.pool.SimpleConnectionPool(1,5,
-                                                    user=conf['USER'],
-                                                    password=conf['PASS'],
-                                                    host=conf['HOST'],
-                                                    port=conf['PORT'],
-                                                    database=conf['DB_NAME']
-                                                )
+    connection_pool = psycopg2.pool.SimpleConnectionPool(
+        1,
+        5,
+        user=conf["USER"],
+        password=conf["PASS"],
+        host=conf["HOST"],
+        port=conf["PORT"],
+        database=conf["DB_NAME"],
+    )
     try:
-        
+
         # Get connection
         connection = connection_pool.getconn()
 
         # Get data from API requests and insert into DB
         with requests.Session() as session:
-            
+
             # Get all the anime updates
             start_anime_list = time.time()
-            url = "https://api.jikan.moe/v4/anime?sfw=true"
-            total_pages = get_page_count(url,session)
-            anime_list = generate_anime_list(session,total_pages)
-            add_anime(anime_list,connection)
+            URL = "https://api.jikan.moe/v4/anime?sfw=true"
+            total_pages = get_page_count(URL, session)
+            anime_list = generate_anime_list(session, total_pages)
+            add_anime(anime_list, connection)
             connection.commit()
             end_anime_list = time.time()
-            logging.info(f"Anime list ETL complete!")
+            logging.info("Anime list ETL complete!")
             logging.info(f"Elapsed time was {end_anime_list-start_anime_list} seconds")
-            
+
             # Get all the anime stat updates
             start_anime_stats = time.time()
             anime_ids = get_anime_ids(connection)
-            upload_anime_stats(anime_ids,connection,session)
+            upload_anime_stats(anime_ids, connection, session)
             connection.commit()
             end_anime_stats = time.time()
-            logging.info(f"Anime stats upload complete!")
-            logging.info(f"Elapsed time was {end_anime_stats-start_anime_stats} seconds")
-            
-        # Commit all changes and clean up staging 
+            logging.info("Anime stats upload complete!")
+            logging.info(
+                f"Elapsed time was {end_anime_stats-start_anime_stats} seconds"
+            )
+
+        # Commit all changes and clean up staging
         insert_anime_scores_and_stats(connection)
         refresh_views(connection)
         clear_staging(connection)
         connection.commit()
-    except (Exception, psycopg2.DatabaseError) as err:
-        logging.info("An error occurred: ",err)
+    except psycopg2.DatabaseError as err:
+        logging.exception("A database error occurred")
 
     finally:
         connection.close()
-        
+
     end = time.time()
     logging.info(f"Done! Elapsed time was {end-start} seconds")
--- extraction_load/myanimelist_api_functions.py
+++ extraction_load/myanimelist_api_functions.py
@@ -1,79 +1,81 @@
-import requests
-import json
-import pandas as pd
-from itertools import count
-from datetime import datetime
-import psycopg2
-
 """
 A collection of functions used to gather anime 
 statistics from My Anime List using the 
 Jikkan API
 """
+import json
+from datetime import datetime,time
+from itertools import count
+import requests
+import psycopg2
+import pandas as pd
 
 
-def get_page_count(url:str,session:requests.Session):
+def get_page_count(url: str, session: requests.Session):
     """
-    Gets the total page count 
+    Gets the total page count
     for the all anime endpoint
     """
     initial_response = session.get(url)
-    total_pages = json.loads(initial_response.text)['pagination']['last_visible_page']
+    total_pages = json.loads(initial_response.text)["pagination"]["last_visible_page"]
     return total_pages
 
 
-def generate_anime_list(session:requests.Session,page_count:int=0):
+def generate_anime_list(session: requests.Session, page_count: int = 0):
     """
     Returns a generator containing pages from the all anime list
     """
-    for page_num in range(1,page_count+1):
+    for page_num in range(1, page_count + 1):
         url = f"https://api.jikan.moe/v4/anime?page={page_num}&sfw=true"
         resp = session.get(url)
         if resp.status_code == 200:
             yield json.loads(resp.text)
-            time.sleep(1) # Space out requests to avoid errors
+            time.sleep(1)  # Space out requests to avoid errors
         else:
             print(f"Error received: {resp.status_code}")
 
 
-def add_anime(anime_list,connection):
+def add_anime(anime_list, connection):
     """
-    Takes in a generator of pages from the 
+    Takes in a generator of pages from the
     generate_anime_list function and a connection,
     and adds any new titles to the database
     """
     cur = connection.cursor()
     for page in anime_list:
-        for anime in page['data']:
+        for anime in page["data"]:
             try:
-                #Insert into database
-                q = """
+                # Insert into database
+                query = """
                     INSERT INTO anime_stage.all_anime
                     (id,title,status,rating,score
                     ,favorites,load_date,airing
                     ,aired_from,aired_to)
                     VALUES
                     (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-                """ 
-                cur.execute(q,(
-                    anime['mal_id'],
-                    anime['title'],
-                    anime['status'],
-                    anime['rating'],
-                    anime['score'],
-                    anime['favorites'],
-                    datetime.now(),
-                    anime['airing'],
-                    anime['aired']['from'],
-                    anime['aired']['to'],  
-                ))
+                """
+                cur.execute(
+                    query,
+                    (
+                        anime["mal_id"],
+                        anime["title"],
+                        anime["status"],
+                        anime["rating"],
+                        anime["score"],
+                        anime["favorites"],
+                        datetime.now(),
+                        anime["airing"],
+                        anime["aired"]["from"],
+                        anime["aired"]["to"],
+                    ),
+                )
             except:
                 print("Error with database connection")
                 raise
     cur.close()
 
 
-def get_anime_stats(session:requests.Session,anime_id:int):
+def get_anime_stats(session: requests.Session, anime_id: int):
     """
     Returns the stats for the anime
     associated with the given anime_id
@@ -84,35 +86,35 @@ def get_anime_stats(session:requests.Session,anime_id:int):
         return json.loads(resp.text)
 
 
-def get_anime_ids(connection:psycopg2.connect):
+def get_anime_ids(connection: psycopg2.connect):
     """
     Takes in a db connection and returns
     a DataFrame of anime ids
     """
-    q = """
+    query = """
         SELECT id
         FROM anime.all_anime
         GROUP BY id
     """
-    ids = pd.read_sql(q,connection)
+    ids = pd.read_sql(query, connection)
     return ids
 
 
-def upload_anime_stats(anime_id_list:pd.DataFrame,
-                    connection:psycopg2.connect,
-                    session:requests.Session):
+def upload_anime_stats(
+    anime_id_list: pd.DataFrame, connection: psycopg2.connect, session: requests.Session
+):
     """
-    Adds the anime stats for each id 
+    Adds the anime stats for each id
     in the given anime_id_list
     """
     cur = connection.cursor()
     # Setup row counter to perform a commit at every 1000 rows
     row = count(1)
-    for id in anime_id_list['id']:
+    for id_ in anime_id_list["id"]:
         # Get the stats
-        stats = get_anime_stats(session,id)
+        stats = get_anime_stats(session, id_)
         rows = next(row)
-        if stats is not None and rows in range(1,1001):
+        if stats is not None and rows in range(1, 1001):
             try:
                 insert_stats_scores = """
                     INSERT INTO 
@@ -121,11 +123,10 @@ def upload_anime_stats(anime_id_list:pd.DataFrame,
                     VALUES 
                         (%s,%s,%s)
                 """
-                cur.execute(insert_stats_scores,
-                            (id,
-                             json.dumps(stats['data']),
-                             datetime.now()),
-                           )
+                cur.execute(
+                    insert_stats_scores,
+                    (id_, json.dumps(stats["data"]), datetime.now()),
+                )
             except:
                 print("Something happened with the connection.")
                 print("Please check and try again")
@@ -137,36 +138,42 @@ def upload_anime_stats(anime_id_list:pd.DataFrame,
     cur.close()
 
 
-def clear_staging(connection:psycopg2.connect):
+def clear_staging(connection: psycopg2.connect):
     """
     Clears the anime_stage tables.
     """
     cursor = connection.cursor()
-    cursor.execute("""
+    cursor.execute(
+        """
         TRUNCATE TABLE anime_stage.all_anime;
-    """)
+    """
+    )
     cursor.close()
 
 
-def refresh_views(connection:psycopg2.connect):
+def refresh_views(connection: psycopg2.connect):
     """
     Refreshes materialized views
     """
     cursor = connection.cursor()
-    cursor.execute("""
+    cursor.execute(
+        """
         REFRESH MATERIALIZED VIEW anime.anime_stats_and_scores;
-    """)
+    """
+    )
     cursor.close()
 
 
-def insert_anime_scores_and_stats(connection:psycopg2.connect):
+def insert_anime_scores_and_stats(connection: psycopg2.connect):
     """
     Inserts anime scores and stats into production
     from staging
     """
     cursor = connection.cursor()
-    cursor.execute("""
+    cursor.execute(
+        """
         SELECT anime_stage.insert_anime_stats();
         SELECT anime_stage.insert_anime_scores();
-    """)
+    """
+    )
     cursor.close()
