7a94189dccdbd865b1cdab51e59b174b4ed15ef8
Martin Arroyo <marroyo7775@gmail.com>
2022-05-12 11:01:23 -0400

    Refactored function code to use context managers for cursors

--- extraction_load/myanimelist_api_functions.py
+++ extraction_load/myanimelist_api_functions.py
@@ -42,38 +42,41 @@ def add_anime(anime_list, connection):
     generate_anime_list function and a connection,
     and adds any new titles to the database
     """
-    cur = connection.cursor()
-    for page in anime_list:
-        for anime in page["data"]:
-            try:
-                # Insert into database
-                query = """
-                    INSERT INTO anime_stage.all_anime
-                    (id,title,status,rating,score
-                    ,favorites,load_date,airing
-                    ,aired_from,aired_to)
-                    VALUES
-                    (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-                """
-                cur.execute(
-                    query,
-                    (
-                        anime["mal_id"],
-                        anime["title"],
-                        anime["status"],
-                        anime["rating"],
-                        anime["score"],
-                        anime["favorites"],
-                        datetime.now(),
-                        anime["airing"],
-                        anime["aired"]["from"],
-                        anime["aired"]["to"],
-                    ),
-                )
-            except:
-                print("Error with database connection")
-                raise
-    cur.close()
+    try:
+        with connection.cursor() as cur:
+            for page in anime_list:
+                for anime in page["data"]:
+                    try:
+                        # Insert into database
+                        query = """
+                            INSERT INTO anime_stage.all_anime
+                            (id,title,status,rating,score
+                            ,favorites,load_date,airing
+                            ,aired_from,aired_to)
+                            VALUES
+                            (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                        """
+                        cur.execute(
+                            query,
+                            (
+                                anime["mal_id"],
+                                anime["title"],
+                                anime["status"],
+                                anime["rating"],
+                                anime["score"],
+                                anime["favorites"],
+                                datetime.now(),
+                                anime["airing"],
+                                anime["aired"]["from"],
+                                anime["aired"]["to"],
+                            ),
+                        )
+                    except Exception as err:
+                        print(f"Error with database connection: {err}")
+                        raise
+    except Exception as err:
+        print(f"Exception occurred while connecting to the database: {err}")
+        raise
 
 
 def get_anime_stats(session: requests.Session, anime_id: int):
@@ -108,61 +111,70 @@ def upload_anime_stats(
     Adds the anime stats for each id
     in the given anime_id_list
     """
-    cur = connection.cursor()
     # Setup row counter to perform a commit at every 1000 rows
     row = count(1)
-    for id_ in anime_id_list["id"]:
-        # Get the stats
-        stats = get_anime_stats(session, id_)
-        rows = next(row)
-        if stats is not None and rows in range(1, 1001):
-            try:
-                insert_stats_scores = """
-                    INSERT INTO 
-                        anime_stage.anime_stats_scores
-                        (anime_id,scores,load_date) 
-                    VALUES 
-                        (%s,%s,%s)
-                """
-                cur.execute(
-                    insert_stats_scores,
-                    (id_, json.dumps(stats["data"]), datetime.now()),
-                )
-            except:
-                print("Something happened with the connection.")
-                print("Please check and try again")
-                raise
-        else:
-            # Perform commit and reset counter
-            connection.commit()
-            row = count(1)
-    cur.close()
+    try:
+        with connection.cursor() as cur:
+            for id_ in anime_id_list["id"]:
+                # Get the stats
+                stats = get_anime_stats(session, id_)
+                rows = next(row)
+                if stats is not None and rows in range(1, 1001):
+                    try:
+                        insert_stats_scores = """
+                            INSERT INTO 
+                                anime_stage.anime_stats_scores
+                                (anime_id,scores,load_date) 
+                            VALUES 
+                                (%s,%s,%s)
+                        """
+                        cur.execute(
+                            insert_stats_scores,
+                            (id_, json.dumps(stats["data"]), datetime.now()),
+                        )
+                    except:
+                        print("Something happened with the connection.")
+                        print("Please check and try again")
+                        raise
+                else:
+                    # Perform commit and reset counter
+                    connection.commit()
+                    row = count(1)
+    except Exception as err:
+        print(f"Exception occurred while connecting to the database: {err}")
+        raise
 
 
 def clear_staging(connection: psycopg2.connect):
     """
     Clears the anime_stage tables.
     """
-    cursor = connection.cursor()
-    cursor.execute(
-        """
-        TRUNCATE TABLE anime_stage.all_anime;
-    """
-    )
-    cursor.close()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                TRUNCATE TABLE anime_stage.all_anime;
+            """
+            )
+    except Exception as err:
+        print(f"Exception occurred while connecting to the database: {err}")
+        raise
 
 
 def refresh_views(connection: psycopg2.connect):
     """
     Refreshes materialized views
     """
-    cursor = connection.cursor()
-    cursor.execute(
-        """
-        REFRESH MATERIALIZED VIEW anime.anime_stats_and_scores;
-    """
-    )
-    cursor.close()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                REFRESH MATERIALIZED VIEW anime.anime_stats_and_scores;
+            """
+            )
+    except Exception as err:
+        print(f"Exception occurred while connecting to the database: {err}")
+        raise
 
 
 def insert_anime_scores_and_stats(connection: psycopg2.connect):
@@ -170,11 +182,14 @@ def insert_anime_scores_and_stats(connection: psycopg2.connect):
     Inserts anime scores and stats into production
     from staging
     """
-    cursor = connection.cursor()
-    cursor.execute(
-        """
-        SELECT anime_stage.insert_anime_stats();
-        SELECT anime_stage.insert_anime_scores();
-    """
-    )
-    cursor.close()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT anime_stage.insert_anime_stats();
+                SELECT anime_stage.insert_anime_scores();
+            """
+            )
+    except Exception as err:
+        print(f"Exception occurred while connecting to the database: {err}")
+        raise
